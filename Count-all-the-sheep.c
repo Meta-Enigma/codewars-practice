f695a79fd5e86a0361606b5d248aa5ddd645566d
Harley <meta_enigma@126.com>
2017-12-27 22:24:32 +0800

    Count all the sheep on farm in the heights of New Zealand

---
+++ Count-all-the-sheep.c
@@ -0,0 +1,37 @@
+/*
+Every week (Friday and Saturday night), the farmer and his son count amount of sheep returned to the yard of their farm.
+
+They count sheep on Friday night, the same goes for Saturday (suppose that sheep returned on Friday are not feeding back on hills on Saturday).
+
+As sheep are not coming in one flock, you will be given two arrays (one for each night) representing number of sheep as
+they were returning to the yard during the evenings (entries are positive ints, higher than zero).
+
+Farmer and his son know the total amount of their sheep, you will be given this number as third parameter.
+
+Your goal is to calculate the amount of sheep lost (not returned) to the farm after Saturday night counting.
+
+Example 1: Input: {1, 2}, {3, 4}, 15 --> Output: 5
+
+Example 2: Input: {3, 1, 2}, {4, 5}, 21 --> Output: 6
+
+Good luck! :-)
+*/
+
+
+// friday and saturday are 0-terminated arrays, i.e. the last element (and only that) will be 0.
+// my answer
+int lostSheep(const int *friday, const int* saturday, int total)
+{
+    int sum = 0;
+    while(*friday){
+        sum += *friday;
+        friday +=1;
+    }
+    while(*saturday){
+        sum += *saturday;
+        saturday +=1;
+    }
+
+    return (total-sum);
+}
+//best answer is mine :)
